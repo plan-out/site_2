852e82ed5c3336d70c71363f388b4b7a0c3b77f0
plan-out <plan_out@naver.com>
2024-08-08 14:07:50 +0900

    2024.08.08 추가작성

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+//ScrollGiveClass플러그인 호출
 var sgc1 = new ScrollGiveClass("header",{
 	baseline:100
+});
+
+//Swiper플러그인 호출
+const swiper1 = new Swiper('#main_view', {
+  // Optional parameters
+  wrapperClass:'slider',
+	slideClass:'view',
+	
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
 });
