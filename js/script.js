b23c316ce725302752f9d37c98609762e49f39e9
plan-out <45305408+plan-out@users.noreply.github.com>
2024-08-14 16:37:51 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -6,11 +6,33 @@ var sgc1 = new ScrollGiveClass("header",{
 //Swiper플러그인 호출
 const swiper1 = new Swiper('#main_view', {
   // Optional parameters
-  wrapperClass:'slider',
+  wrapperClass:'slider',  
 	slideClass:'view',
-	
-  // If we need pagination
+	autoplay: { //자동슬라이드
+    delay: 5000, //하나의 슬라이드가 보여주는 시간 
+  },
+  speed:800, //슬라이드가 전환되는(움직이는) 시간
+  spaceBetween:10, //슬라이드 사이간격
+  // If we need pagination  
   pagination: {
-    el: '.swiper-pagination',
+    el: '.pager', //버튼영역 대상
+    bulletActiveClass:'active', //활성화된 요소에 추가되는 클래스
+    clickable:true, //클릭시 슬라이드 이동
   },
 });
+
+//press영역 스와이퍼 호출
+let swiper2 = new Swiper("#press .inner",{
+  wrapperClass:'slider',  
+	slideClass:'item', 
+  slidesPerView:5,
+  spaceBetween:30,
+  pagination:{
+    el:'.dot',
+    bulletActiveClass:'active',
+    clickable:true
+  }
+});
+
+
+
